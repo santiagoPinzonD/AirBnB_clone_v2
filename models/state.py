605e1213844cc7f902b9c654183d1cbf133166e7
santiagopinzonD <santiagopinzon456@gmail.com>
2020-12-17 05:56:56 +0000

    change state and condition

--- models/state.py
+++ models/state.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
 import models
-from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column("name", String(128), nullable=True)
-    if getenv('HBNB_TYPE_STORAGE') == "bd":
-        cities = relationship("City", cascade='all, delete', backref='state')
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
 
-    @property
-    def cities(self):
-        """getter"""
-        inst = []
-        dic = models.storage.all(City)
-        for k, v in dic.items():
-            if v.state_id == self.id:
-                inst.append(v)
-        return inst
+    if os.getenv('HBNB_TYPE_STORAGE') == 'fs':
+        @property
+        def cities(self):
+            _cities = []
+            for _id, city in models.storage.all(City).items():
+                if self.id == city.state_id:
+                    _cities.append(city)
+            return _cities
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete")
