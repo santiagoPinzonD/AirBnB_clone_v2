eb4d0e38b7a8813a2e5564212683d7528b4f2ac5
santiagopinzonD <santiagopinzon456@gmail.com>
2020-12-18 03:46:49 +0000

    changing order

--- models/amenity.py
+++ models/amenity.py
@@ -8,4 +8,4 @@ from sqlalchemy import Column, String
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", back_populates="amenities")
+    place_amenities = relationship("Place", secondary='place_amenity')
--- models/place.py
+++ models/place.py
@@ -6,14 +6,22 @@ from sqlalchemy.orm import relationship, backref
 import os
 from sqlalchemy import MetaData, Table
 
+metadata = Base.metadata
+
+place_amenity = Table('place_amenity', metadata,
+                      Column('place_id', String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """This class define a table with various atributtes"""
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"),
-                     nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"),
-                     nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
@@ -24,17 +32,14 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    metadata = Base.metadata
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship("Review", backref="place",
+                               cascade="all, delete")
+        amenities = relationship("Amenity", backref="place",
+                                 cascade="all, delete", viewonly=False,
+                                 secondary="place_amenity")
 
-    place_amenity = Table('place_amenity', metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True, nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True, nullable=False))
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'fs':
+    else:
         @property
         def place(self):
             """getter method place"""
@@ -58,10 +63,3 @@ class Place(BaseModel, Base):
             """setter method"""
             if type(value) is 'Amenity':
                 self.amenity_ids.append(value.id)
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        reviews = relationship("Review", backref="place",
-                               cascade="all, delete")
-        amenities = relationship("Amenity", backref="place",
-                                 cascade="all, delete", viewonly=False,
-                                 secondary="place_amenity")
