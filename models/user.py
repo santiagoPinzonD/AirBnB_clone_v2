8d38a00b72f021a2cbc7955fb48dec4fa0452651
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-18 00:57:34 +0000

    nullable=True

--- models/user.py
+++ models/user.py
@@ -9,5 +9,5 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column("email", String(128), nullable=False)
     password = Column("password", String(128), nullable=False)
-    first_name = Column("first_name", String(128), nullable=False)
-    last_name = Column("last_name", String(128), nullable=False)
+    first_name = Column("first_name", String(128), nullable=True)
+    last_name = Column("last_name", String(128), nullable=True)
