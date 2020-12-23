da297aa68a73f5953ef402cd1813ed40c15c2e3e
santiagopinzonD <santiagopinzon456@gmail.com>
2020-12-23 03:52:45 +0000

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,18 +11,18 @@ env.host = ['35.227.48.225', '34.74.219.148']
 
 def do_deploy(archive_path):
     """create function of deploy"""
-    pathfix = archive_path.split("/")[-1]
-    whithouttgz = pathfix.split(".")[0]
-    path = "/data/web_static/releases"
     if os.path.exists(archive_path) is False:
         return False
     try:
+        pathfix = archive_path.split("/")[-1]
+        whithouttgz = pathfix.split(".")[0]
+        path = "/data/web_static/releases"
         put("{}".format(archive_path), "/tmp/{}".format(pathfix))
-        run("sudo mkdir -p {}/{}" .format(path, whithouttgz))
+        run("sudo mkdir -p {}/{}/" .format(path, whithouttgz))
         run("sudo tar -xzf /tmp/{} -C {}/{}/" .format(pathfix,
                                                      path, whithouttgz))
         run("sudo rm /tmp/{}".format(pathfix))
-        run("sudo mv {}/{}/web_static/* {}/{}".format(path,
+        run("sudo mv {}/{}/web_static/* {}/{}/".format(path,
                                                       whithouttgz, path, whithouttgz))
         run("sudo rm -rf {}/{}/web_static" .format(path, whithouttgz))
         run("sudo rm -rf /data/web_static/current")
