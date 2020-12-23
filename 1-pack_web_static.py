dae4e50ebd35bcd81bf1748af66e8b1e7f22a6c5
santiagopinzonD <santiagopinzon456@gmail.com>
2020-12-23 00:56:34 +0000

    add task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that generates a .tgz archive
+"""
+
+from datetime import datetime
+from fabric.api import run, cd, local
+
+
+def do_pack():
+    """func that create a dir and a file """
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    path = "versions/web_static_{}.tgz" .format(time)
+    var = local("tar -czvf {} web_static/" .format(path))
+    if var == 0:
+        print(path)
+    else:
+        return None
