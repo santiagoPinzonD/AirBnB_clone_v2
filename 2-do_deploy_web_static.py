7a136634dd839dbb5deb167b03e20e25192226d6
santiagopinzonD <santiagopinzon456@gmail.com>
2020-12-23 03:39:44 +0000

    making changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,21 +12,22 @@ env.host = ['35.227.48.225', '34.74.219.148']
 def do_deploy(archive_path):
     """create function of deploy"""
     pathfix = archive_path.split("/")[-1]
-    whithouttgz = pathfix.split(".")
+    whithouttgz = pathfix.split(".")[0]
     path = "/data/web_static/releases"
     if os.path.exists(archive_path) is False:
         return False
     try:
         put("{}".format(archive_path), "/tmp/{}".format(pathfix))
-        run("sudo mkdir -p {}/{}/" .format(path, whithouttgz[0]))
-        run("sudo tar -xzf /tmp/{} -C {}/{}" .format(pathfix,
-                                                     path, whithouttgz[0]))
+        run("sudo mkdir -p {}/{}" .format(path, whithouttgz))
+        run("sudo tar -xzf /tmp/{} -C {}/{}/" .format(pathfix,
+                                                     path, whithouttgz))
         run("sudo rm /tmp/{}".format(pathfix))
         run("sudo mv {}/{}/web_static/* {}/{}".format(path,
-                                                      pathfix, path, pathfix))
+                                                      whithouttgz, path, whithouttgz))
         run("sudo rm -rf {}/{}/web_static" .format(path, whithouttgz))
         run("sudo rm -rf /data/web_static/current")
-        run("ln -s {}/{}/ /data/web_static/current" .format(path, whithouttgz))
+        run("sudo ln -sf {}/{}/ /data/web_static/current" .format(path, whithouttgz))
+        print("New version deployed!")
         return True
     except Exception:
         return False
