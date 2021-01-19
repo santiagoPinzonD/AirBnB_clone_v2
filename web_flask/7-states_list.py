4af78d9dd6a60157929e00332a05a6f4b360c939
santiagopinzonD <santiagopinzon456@gmail.com>
2021-01-19 20:42:35 +0000

    fix pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,6 +7,7 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def displayHTML():
     """display a HTML page: (inside the tag BODY)"""
